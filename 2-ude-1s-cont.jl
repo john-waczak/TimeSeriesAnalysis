0704ff0131584cd3ffc8c8fb24406b3a0c85f7a2
John Waczak <john.louis.waczak@gmail.com>
2024-05-05 12:16:50 -0500

    updated plots

--- 2-ude-1s-cont.jl
+++ 2-ude-1s-cont.jl
@@ -34,7 +34,7 @@ if !ispath(figpath)
 end
 @assert ispath(figpath)
 
-fig_savepath = joinpath(figpath, "HAVOK-cont")
+fig_savepath = joinpath(figpath, "UDE-cont")
 if !ispath(fig_savepath)
     mkpath(fig_savepath)
 end
@@ -56,7 +56,7 @@ x_tick_pos = [Second(d - t1).value for d ∈ x_tick]
 x_tick_strings = [Dates.format.(d, "dd/mm/yy") for d ∈ x_tick]
 
 
-params_path = joinpath(fig_savepath, "havok_params.json")
+params_path = joinpath("./figures", "central-nodes", "central-hub-4", "HAVOK-cont", "havok_params.json")
 params_dict = JSON.parsefile(params_path)
 
 
@@ -122,17 +122,20 @@ forcing(t) = [itp(t) for itp ∈ itps]
 
 
 # Let's try to train a UDE to learn the missing physics:
+
+# scale_factor = 10^(round(log10(maximum(abs.(A * X[:, 1:r_model]')))))
+# scale_factor = 5*10^(round(log10(maximum(abs.(A * X[:, 1:r_model]')))) - 1)
+scale_factor = 10^(round(log10(maximum(abs.(A * X[:, 1:r_model]')))) - 1)
+# scale_factor = 5*10^(round(log10(maximum(abs.(A * X[:, 1:r_model]')))) - 2)
+
 rbf(x) = exp.(-(x.^2))
 out_func(x) = scale_factor * tanh(x)
 act_func = tanh
 
-# estimate output scale factor for nonlinear-terms so that
-# so that correction is weaker than A
-scale_factor = 10^(round(log10(maximum(abs.(A * X[:, 1:r_model]')))) - 1)
 
 
 
-const n_hidden = 20
+const n_hidden = 30
 const NN = Lux.Chain(
     Lux.Dense(r_model, n_hidden, act_func),
     Lux.Dense(n_hidden, n_hidden, act_func),
@@ -168,7 +171,7 @@ function get_Zs_from_X(X)
 end
 
 # set up ODE problem
-idx_train_short = 1:(15*60)
+idx_train_short = 1:(5*60)
 # idx_train_short = 1:90
 ts_train_short = ts_x[idx_train_short]
 x₀ = X[1,1:r_model]
@@ -187,16 +190,6 @@ X̂hk = Array(sol)
 Ẑs = get_Zs_from_X(X̂)
 Ẑs_hk = get_Zs_from_X(X̂hk)
 
-fig = Figure();
-ax = CairoMakie.Axis(fig[2,1], xlabel="t", ylabel="PM 2.5");
-l_orig = lines!(ax, ts_train_short, Zs_x[idx_train_short], linewidth=3)
-l_havok = lines!(ax, ts_train_short, Ẑs_hk, linestyle=:dot, linewidth=2)
-l_ude = lines!(ax, ts_train_short, Ẑs, linestyle=:dash, linewidth=2)
-fig[1,1] = Legend(fig, [l_orig, l_havok, l_ude], ["Original", "HAVOK", "UDE"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
-fig
-
-
-
 
 # set up function for generating solution with new parameters
 function predict(θ, x0_new = X[1, 1:r_model], T = ts_train_short)
@@ -207,14 +200,14 @@ end
 
 predict(p)
 
+
+idx_ude_train = 1:(5*60)  # 5 minutes
+
 function loss(θ)
-    X̂ = predict(θ)
+    X̂ = predict(θ, X[1,1:r_model], ts_x[idx_ude_train])
     Ẑs = get_Zs_from_X(X̂)
-    mean(abs2, Ẑs .- Zs_x[idx_train_short])
+    sqrt(mean((Ẑs .- Zs_x[idx_ude_train]) .^2 ))
 end
-
-loss(p)
-
 
 
 # set up array to track loss function with callback
@@ -230,38 +223,238 @@ function callback(p, l)
 end
 
 
-# Build Optimization Problem for Training
+
+# visualize the fit
+Ẑs_fit = get_Zs_from_X(predict(p))
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[2,1], xlabel="t (min)", ylabel="PM 2.5");
+l_orig = lines!(ax, ts_train_short ./ 60, Zs_x[idx_train_short], linewidth=3)
+l_havok = lines!(ax, ts_train_short ./ 60, Ẑs_hk, linestyle=:dot, linewidth=2)
+l_ude = lines!(ax, ts_train_short ./ 60, Ẑs_fit, linestyle=:dash, linewidth=2)
+fig[1,1] = Legend(fig, [l_orig, l_havok, l_ude], ["Original", "HAVOK", "HAVOK + NN"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
+
+save(joinpath(fig_savepath, "1__UDE-fit-NN-pre-training.png"), fig)
+save(joinpath(fig_savepath, "1__UDE-fit-NN-pre-training.pdf"), fig)
+
+fig
+
+
+
+
+
 adtype = Optimization.AutoZygote()
+p_best = p
+
+# First training round using ADAM
 optf = Optimization.OptimizationFunction((x,p) -> loss(x), adtype)
-# ComponentVector is used here to flatten the parameters in an easy way with views
 optprob = Optimization.OptimizationProblem(optf, ComponentVector{Float64}(p))
+res = Optimization.solve(optprob, ADAM(), callback=callback, maxiters=10_000)
+p = res.u
 
 
-# Train first using ADAM for rapid convergence to local minimum
-# res1 = Optimization.solve(optprob, ADAM(), callback=callback, maxiters=1)
-# res1 = Optimization.solve(optprob, ADAM(), callback=callback, maxiters=1000)
-res1 = Optimization.solve(optprob, ADAM(), callback=callback, maxiters=5000)
-save(joinpath(fig_savepath, "model_params.jld2"), Dict("ps_trained" => res1.u, "st" => _st))
+# Second training round using LBFGS
+optf = Optimization.OptimizationFunction((x,p) -> loss(x), adtype)
+optprob = Optimization.OptimizationProblem(optf, ComponentVector{Float64}(p))
+res = Optimization.solve(optprob, LBFGS(), callback=callback, maxiters=1_000)
+p = res.u
 
-optprob2 = Optimization.OptimizationProblem(optf, res1.u)
-res2 = Optimization.solve(optprob2, Optim.LBFGS(), callback=callback, maxiters=1000)
 
-save(joinpath(fig_savepath, "model_params.jld2"), Dict("ps_trained" => res2.u, "st" => _st))
+save(joinpath(fig_savepath, "model_params.jld2"), Dict("ps_trained" => p, "st" => _st, "A" => A, "B" => B, "scale_factor" => scale_factor, "Ur" => Ur, "σr" => σr, "Vr" => Vr))
 
-# Rename the best candidate
-p_trained = res2.u
+
+
+# plot the losses:
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1], xlabel="iteration", ylabel="RMSE", title="UDE Training Loss", yscale=log10, xscale=log10)
+lines!(ax, 1:length(losses), losses)
+fig
+
+
+save(joinpath(fig_savepath, "2__nn-training-loss.png"), fig)
+save(joinpath(fig_savepath, "2__nn-training-loss.pdf"), fig)
 
 
 # visualize the fit
-Ẑs_fit = get_Zs_from_X(predict(p_trained))
+Ẑs_fit = get_Zs_from_X(predict(p))
 fig = Figure();
-ax = CairoMakie.Axis(fig[2,1], xlabel="t", ylabel="PM 2.5");
-l_orig = lines!(ax, ts_train_short, Zs_x[idx_train_short], linewidth=3)
-l_havok = lines!(ax, ts_train_short, Ẑs_hk, linestyle=:dot, linewidth=2)
-l_ude = lines!(ax, ts_train_short, Ẑs_fit, linestyle=:dash, linewidth=2)
+ax = CairoMakie.Axis(fig[2,1], xlabel="t (min)", ylabel="PM 2.5");
+l_orig = lines!(ax, ts_train_short ./ 60, Zs_x[idx_train_short], linewidth=3)
+l_havok = lines!(ax, ts_train_short ./ 60, Ẑs_hk, linestyle=:dot, linewidth=2)
+l_ude = lines!(ax, ts_train_short ./ 60, Ẑs_fit, linestyle=:dash, linewidth=2)
 fig[1,1] = Legend(fig, [l_orig, l_havok, l_ude], ["Original", "HAVOK", "UDE"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
+
+
 fig
 
+
+# now we should evaluate on a longer time-frame, say 15-min
+idx_15 = 1:(15*60)
+Ẑs_fit = get_Zs_from_X(predict(p_trained, X[1,1:r_model], ts_x[idx_15]))
+fig = Figure();
+ax = CairoMakie.Axis(fig[2,1], xlabel="t (min)", ylabel="PM 2.5");
+l_orig = lines!(ax, ts_x[idx_15]./ 60, Zs_x[idx_15], linewidth=3)
+# l_havok = lines!(ax, ts_x[idx_15]./ 60, Ẑs_hk, linestyle=:dot, linewidth=2)
+l_ude = lines!(ax, ts_x[idx_15] ./ 60, Ẑs_fit, linestyle=:dash, linewidth=2)
+#fig[1,1] = Legend(fig, [l_orig, l_havok, l_ude], ["Original", "HAVOK", "UDE"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
+fig[1,1] = Legend(fig, [l_orig, l_ude], ["Original", "UDE"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
+
+save(joinpath(fig_savepath, "3__UDE-fit-NN-trained.png"), fig)
+save(joinpath(fig_savepath, "3__UDE-fit-NN-trained.pdf"), fig)
+
+fig
+
+
+
+# visualize forcing function time series
+fig = Figure();
+idx_plot = 1:(60*60)
+ax = CairoMakie.Axis(fig[1,1], xlabel="t (min)", ylabel="forcing")
+lines!(ax, ts_x[idx_plot] ./ 60, vcat(forcing.(ts_x[idx_plot])...))
+save(joinpath(fig_savepath, "4__forcing-timeseries.png"), fig)
+save(joinpath(fig_savepath, "4__forcing-timeseries.pdf"), fig)
+fig
+
+xlims!(ax, ts_x[1] / 60, 15)
+save(joinpath(fig_savepath, "5__forcing-timeseries-15min.png"), fig)
+save(joinpath(fig_savepath, "5__forcing-timeseries-15min.pdf"), fig)
+fig
+
+
+
+
+
+# --------------------------------------------------------------------------------------------
+#   Do Sparse Regression on Learned NN
+# --------------------------------------------------------------------------------------------
+
+
+# Set up sparse regression problem
+# the Dataset may be too big for this to be efficient...
+@variables x[1:r_model]
+# b = polynomial_basis(x, 2);
+b_2 = []
+b_3 = []
+for i ∈ 1:r_model
+    for j ∈ 1:r_model
+        if j ≥ i
+            push!(b_2, x[i]*x[j])
+        end
+    end
+end
+
+for i ∈ 1:r_model
+    for j ∈ 1:r_model
+        for k ∈ 1:r_model
+            if (j ≥ i) && (k ≥ j)
+                push!(b_3, x[i]*x[j]*x[k])
+            end
+        end
+    end
+end
+
+# basis = Basis(vcat(b_2, b_3), x);
+basis = Basis(b_2, x);
+
+
+# create extra data by integrating trained ODE at a finer timestep
+ts_reg = ts_train_short[1]:0.5:ts_train_short[end]
+
+X̂ = predict(p, X[1, 1:r_model], ts_reg)
+Ŷ = NN(X̂, p, _st)[1]
+
+size(X̂)
+size(Ŷ)
+
+nn_prob = DirectDataDrivenProblem(X̂, Ŷ, name= :UDE_REG)
+
+
+λ = exp10.(-1:0.025:3)
+# opt = STLSQ(0.125)   # Nonzero percent: 69.2
+opt = STLSQ(λ)  # Nonzero percent: 88.1
+
+# λ          | %
+# -------------------
+# -1:0.05:1  | 19.436
+# -1:0.025:3 | 19.141
+# -1:0.02:3  | 19.196
+# -1:0.1:2   | 20.47
+# -1:0.1:1   | 20.47
+# -1:0.1:0   | 26.4
+# 0.1        | 63.2
+# 0.01       | 94.4
+
+options = DataDrivenCommonOptions(
+    maxiters = 10_000,
+    normalize = DataNormalization(),  # ZScoreTransform caused failure
+    selector = aicc, # 20.47
+    #selector = bic,  # 20.997
+    #selector = aic,   # 21.38
+    #selector = rss,   # 32.779
+    digits = 10,
+    data_processing = DataProcessing(
+        split = 0.9,
+        # batchsize=32,  # 22.35
+        # batchsize=28,  # 18.11
+        batchsize=26,  # 17.174
+        shuffle=true,
+        rng=StableRNG(42)
+    )
+)
+
+
+nn_res = solve(nn_prob, basis, opt, options=options)
+
+# evaluate the result
+nn_eqs = get_basis(nn_res)
+p_nn = get_parameter_values(nn_eqs)
+(length(p_nn)/(length(basis) * r_model))*100
+
+
+
+# Now that we have equation candidates, learn new parameters to make this work:
+function recovered_dynamics!(dx, x, p, t, A, B)
+    dx .= A*x + B*forcing(t) + nn_eqs(u, p)
+end
+
+# define closure
+rec_dynamics!(dx, x, p, t) = recovered_dynamics!(dx, x, p, t, A, B)
+
+function parameter_loss(p)
+    Y = reduce(hcat, map(Base.Fix2(nn_eqs, p), eachcol(X̂)))
+    sum(abs2, Ŷ .- Y)
+end
+
+
+
+# THIS SEEMS REALLY SLOW
+
+# losses = Float64[]
+# optf = Optimization.OptimizationFunction((x,p) -> parameter_loss(x), adtype)
+# optprob = Optimization.OptimizationProblem(optf, get_parameter_values(nn_eqs))
+# parameter_res = Optimization.solve(optprob, Optim.LBFGS(), callback=callback, maxiters=1000)
+
+
+
+
+
+
+# tspan = (ts_x[1], ts_x[idx_15[end]])
+# prob_rec = ODEProblem(rec_dynamics!, x₀, tspan, p_nn)
+# sol = solve(prob_nn, saveat=ts_x[idx_15]);
+# X̂ = Array(sol)
+# Ẑs_rec = get_Zs_from_X(X̂)
+
+
+
+# # okay, so these values aren't great... let's try and optimize the equations we recovered
+# fig = Figure();
+# ax = CairoMakie.Axis(fig[2,1], xlabel="t (min)", ylabel="PM 2.5");
+# l_orig = lines!(ax, ts_train_short ./ 60, Zs_x[idx_train_short], linewidth=3)
+# l_havok = lines!(ax, ts_train_short ./ 60, Ẑs_hk, linestyle=:dot, linewidth=2)
+# l_rec = lines!(ax, ts_train_short ./ 60, Ẑs_rec[idx_train_short], linestyle=:dashdot, linewidth=2)
+# fig[1,1] = Legend(fig, [l_orig, l_havok, l_rec], ["Original", "HAVOK", "Recovered UDE"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=17, height=-5)
+# fig
 
 
 
